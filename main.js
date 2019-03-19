13f18d76c2a4267d9628fd8fa3b653586d1db5f4
JDode <2420498526@qq.com>
2019-03-19 14:35:59 +0800

    修改启动页加载背景👏

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow() {
     // 创建浏览器窗口。
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 540, height: 320, frame: false, resizable: false })
 
     // 然后加载应用的 index.html。
     win.loadFile('welcome.html')
