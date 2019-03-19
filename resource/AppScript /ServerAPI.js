c2aa4142c15fa1d3912d6198cfeddbffb3e37884
JDode <2420498526@qq.com>
2019-03-19 22:06:55 +0800

    修改界面渲染⚠️

---
+++ resource/AppScript /ServerAPI.js
@@ -0,0 +1,6 @@
+//服务器API请求类
+const ServerAPI = {
+    welcome_api = "",
+    update_api =  "",
+    version_api = "",
+}
---
+++ resource/AppScript /welcome.js
@@ -0,0 +1,34 @@
+//welcome对象
+let welcome = {
+    content: "Welclome this App.",
+    icon: "./ticon.png",
+    mpegUrl: "http://news.iciba.com/admin/tts/2019-03-19-day.mp3",
+    //获取最新版本
+    version: function (event) {
+
+    },
+    //检查更新
+    update: function (params) {
+
+    },
+    //获取服务器数据
+    getServerData: function (event) {
+
+    }
+};
+//创建新窗口函数
+const { BrowserWindow } = require('electron').remote
+const path = require('path')
+const newWindowBtn = document.getElementById('Play')
+newWindowBtn.addEventListener('click', (event) => {
+    //创建audio对象为下面播放🎵音乐作准备
+    let audio = new Audio()
+    audio.src = "http://news.iciba.com/admin/tts/2019-03-19-day.mp3"
+    audio.play();
+    const modalPath = path.join('file://', __dirname, './main.html')
+    let win = new BrowserWindow({ width: 860, height: 620 })
+    win.on('close', () => { win = null })
+    win.loadURL(modalPath)
+    win.show()
+    this.close();
+})
