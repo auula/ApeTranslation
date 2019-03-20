a426125c3d8db2ee64635640dd0a612910d8166d
JDode <2420498526@qq.com>
2019-03-20 19:07:50 +0800

    🌶清理git缓存

--- README.md
+++
@@ -1,2 +0,0 @@
-# ApeTranslation
-一个使用Electron技术💁‍♂️开发的跨平台桌面操作系统强大的翻译程序🔥
--- resource/JScript /ServerAPI.js
+++
@@ -1,7 +0,0 @@
-
-//服务器API请求类
-const ServerAPI = {
-    welcome_api: "https://codegc.me/welcome.json",
-    update_api: "",
-    version_api: "",
-}
