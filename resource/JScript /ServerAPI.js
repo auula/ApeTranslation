3ab5bbc42f80776f0469150192de679939398544
JDode <2420498526@qq.com>
2019-03-24 16:01:21 +0800

    🔥绘制about界面的UI🛫

--- resource/JScript /ServerAPI.js
+++ resource/JScript /ServerAPI.js
@@ -1,6 +1,6 @@
 //服务器API请求类
 const ServerAPI = {
-    welcome_api: "http://localhost:8080/api",
+    welcome_api: "http://localhost:9098/api",
     update_api: "",
     version_api: "",
 }   
---
+++ resource/JScript /appMain.js
@@ -0,0 +1,38 @@
+//引入一些必备的组件
+const { BrowserWindow, app } = require('electron').remote
+const path = require('path')
+const http = require('http')
+var vue = new Vue({
+    el: "#content-main",
+    data: {
+        trans: false,
+        about: true,
+    }
+});
+
+
+
+
+function Exit() {
+    mdui.dialog({
+        title: '⚠️系统提示:',
+        content: '你是想退出软件吗？如果想点击“退出”或者“取消”',
+        buttons: [
+            {
+                text: '取消'
+            },
+            {
+                text: '退出',
+                onClick: function (inst) {
+                    //console.log("infoLog:", "销毁App进程", new Date())
+                    Quit();
+                }
+            }
+        ]
+    });
+}
+//销毁App进程
+function Quit() {
+    const { app } = require('electron').remote
+    app.quit();
+}
